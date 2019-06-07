3fa9d627955aa7cbb528969dec369ff6d0ce6c05
Heidi Williams-Foy <foywilliams@gmail.com>
2019-06-07 18:06:31 -0400

    denote developer key expiration

--- app.py
+++ app.py
@@ -2,33 +2,28 @@
 import requests
 import json
 
-
 while True:
+ # url to get the list of event entries
+ url = "https://api.box.com/2.0/events"
+ payload = {'stream_position': 'now'}
 
-    # url to get the list of event entries
-    url = "https://api.box.com/2.0/events"
-    payload = {'stream_position': 'now'}
-
-    #headers with user authorization token to authenticate api calls
-    headers = {'Authorization': 'Bearer JhUIh90Yj5EYJzu6dXuFB2Yu72PwUS9T'}
-    # options request to event url to get list of events
-    r1 = requests.options(url, headers=headers)
-    events = r1.json()
-    #extract stream_url from the retured event entry
-    stream_url = events['entries'][0]['url']
-
-
-
-    print('real time url '+ stream_url)
-    print('long polling...')
-    # sending get request and saving the response as response object
-    # get request to stream endpoint
-    r = requests.get(stream_url, headers=headers)
-    # extracting data in json format
-    data = r.json()
-    print(data['message'])
-    print('long polling...')
-    print('reconnect')
+#headers with user authorization token to authenticate api calls
+#developer keys will expire within 60 minutes. You will want to go to Box Developers > Configuration to generate your own key
+headers = {'Authorization': 'Bearer YR8dDKJqjbyqWJno6r8NoivyTmilTobz'}
+# options request to event url to get list of events
+r1 = requests.options(url, headers=headers)
+events = r1.json()
+#extract stream_url from the retured event entry
+stream_url = events['entries'][0]['url']
 
 
-
+print('real time url '+ stream_url)
+print('long polling...')
+# sending get request and saving the response as response object
+# get request to stream endpoint
+r = requests.get(stream_url, headers=headers)
+# extracting data in json format
+data = r.json()
+print(data['message'])
+print('long polling...')
+print('reconnect')
